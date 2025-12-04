031dba4a865c310676a7f90cc1755585d4e6faff
VadimLatkin <ssshhhiiittt126783@gmail.com>
2025-12-04 17:48:08 +0300

    Добавил контекст процессор

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cart.context_processors.cart',
+                'catalog.context_processors.categories'
             ],
         },
     },
