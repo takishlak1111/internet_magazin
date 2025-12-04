ca48ea705a8e89e45d90af6ba2ee92dd3d6d0f10
VadimLatkin <ssshhhiiittt126783@gmail.com>
2025-12-04 18:16:46 +0300

    Добавил шаблоны каталога

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from .models import Product, Category, Brand
 
-# Create your views here.
+
+def product_list(request):
+    """Главная страница каталога - все товары"""
+    products = Product.objects.filter(is_active=True)
+    return render(request, 'catalog/product_list.html', {'products': products})
+
+
+def product_detail(request, slug):
+    """Страница одного товара"""
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+    return render(request, 'catalog/product_detail.html', {'product': product})
+
+
+def category_detail(request, slug):
+    """Товары в категории"""
+    category = get_object_or_404(Category, slug=slug)
+    products = Product.objects.filter(category=category, is_active=True)
+    return render(request, 'catalog/category_detail.html', {
+        'category': category,
+        'products': products
+    })
+
+
+def brand_detail(request, slug):
+    """Товары бренда"""
+    brand = get_object_or_404(Brand, slug=slug)
+    products = Product.objects.filter(brand=brand, is_active=True)
+    return render(request, 'catalog/brand_detail.html', {
+        'brand': brand,
+        'products': products
+    })
+
+
+def product_search(request):
+    """Поиск товаров"""
+    query = request.GET.get('q', '')
+    products = Product.objects.filter(is_active=True)
+
+    if query:
+        products = products.filter(
+            Q(product_name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(category__name__icontains=query) |
+            Q(brand__name__icontains=query)
+        )
+
+    return render(request, 'catalog/product_search.html', {
+        'products': products,
+        'query': query
+    })
