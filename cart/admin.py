0b795911c51f5f2f7b3f83506304c253b36d907e
VadimLatkin <ssshhhiiittt126783@gmail.com>
2025-12-09 17:34:11 +0300

    написал admin для корзины

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'session', 'item_count', 'total']
+
+    def item_count(self, obj):
+        return obj.items.count()
+
+    item_count.short_description = 'Товаров'
+
+    def total(self, obj):
+        return obj.total()
+
+    total.short_description = 'Сумма'
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'cart', 'quantity', 'total']
+
+    def total(self, obj):
+        return obj.total()
