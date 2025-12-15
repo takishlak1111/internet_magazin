ee20b65b39314edcee53282913e647e88b00eabf
VadimLatkin <ssshhhiiittt126783@gmail.com>
2025-12-15 22:41:54 +0300

    исправил ошибки слияния

--- users/views.py
+++ users/views.py
@@ -2,14 +2,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout # тут чтобы не ругалось на фунции(одно имя)
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.urls import reverse
 from cart.models import Cart, CartItem
 from .forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-<<<<<<< HEAD
     """
     Обрабатывает аутентификацию пользователя.
 
@@ -20,14 +19,11 @@ def login(request):
         HttpResponse: Страница входа или редирект на главную при успешной аутентификации.
     """
     if request.method == 'POST':
-=======
-    if request.method == 'POST': # Post - чисто данные формы 
->>>>>>> a09abf6e971940624d1cd7c4ed88851f911fdd37
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
-            user = authenticate(username=username, password=password) # ищем пароль и юз в бд
+            user = authenticate(username=username, password=password)
             if user:
                 auth_login(request, user)
                 return HttpResponseRedirect(reverse('catalog:product_list'))
@@ -55,7 +51,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            user = form.instance # доступ к созданной форме
+            user = form.instance
             auth_login(request, user)
             return HttpResponseRedirect(reverse('catalog:product_list'))
     else:
@@ -68,7 +64,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
-@login_required # декоратор для проверки вошел ли пользователь чтобы смотреть защищенные стр
+@login_required
 def profile(request):
     """
     Отображает и обрабатывает редактирование профиля пользователя.
@@ -82,22 +78,9 @@ def profile(request):
         HttpResponse: Страница профиля пользователя.
     """
     user = request.user
-<<<<<<< HEAD
 
     cart, created = Cart.objects.get_or_create(user=user)
 
-=======
-    
-    
-    cart, created = Cart.objects.get_or_create(user=user) # ищет корзину или создает ее
-    #try:
-        #cart = Cart.objects.get(user=user)  # Пытаемся найти
-        #created = False
-    #except Cart.DoesNotExist:  # Если не нашли
-        #cart = Cart.objects.create(user=user)  # Создаем
-        #created = True
-    
->>>>>>> a09abf6e971940624d1cd7c4ed88851f911fdd37
     cart_items = CartItem.objects.filter(cart=cart)
     cart_total = cart.total() if cart_items.exists() else 0
 
@@ -107,11 +90,11 @@ def profile(request):
             form.save()
             messages.success(request, 'Профиль успешно обновлен!')
             return redirect('users:profile')
-        else: # выдаем по какому полю какая ошибка
+        else:
             for field, errors in form.errors.items():
                 for error in errors:
                     messages.error(request, f'{field}: {error}')
-    else: # это если get запрос(получение данных)
+    else:
         form = ProfileForm(instance=user)
 
     context = {
